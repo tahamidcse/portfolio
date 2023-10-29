0b8df72b709d6be9a0acc0d03723a01ff32e3c26
tahamidcse <112802620+tahamidcse@users.noreply.github.com>
2023-10-29 23:48:58 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// You can add JavaScript interactivity here if needed
+// For example, smooth scrolling for anchor links
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
